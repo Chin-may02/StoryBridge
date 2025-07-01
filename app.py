5afab7ecd138cb9e0878f01db580409905e0d869
djv554 <vazdeanne5@gmail.com>
2025-07-01 23:35:08 +0530

    updated requirements

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from urllib.parse import quote
 import os
 from functools import lru_cache
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
+
+# from requests.packages.urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
--- venn/Scripts/activate.bat
+++ venn/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\gbrhi\Downloads\storybridge_project\venn"
+set "VIRTUAL_ENV=C:\Users\Deanne Vaz\Desktop\mlweb\StoryBridge\venn"
 
 if not defined PROMPT set PROMPT=$P$G
 
